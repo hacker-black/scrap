819714ef24ad0f7d80dd5ee1e93b02fca9a67120
Hossein Heydari <hossein7heydari@gmail.com>
2016-04-14 21:03:40 +0430

    Create scrap.py

---
+++ scrap.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Getting libraries
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+# Loading page
+url = urlopen("http://ubuntuhandbook.org/")
+# Geting last posts name
+html = BeautifulSoup(url.read(), 'html.parser')
+for web in html.find_all("h2"):
+    print(web.get_text())
